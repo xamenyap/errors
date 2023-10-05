9764a42b6465fe2a757cc127167552bf16eb4576
xamenyap <jpham2811@gmail.com>
2023-10-05 21:02:57 +0800

    Add documentation comments

--- errors.go
+++ errors.go
@@ -4,12 +4,16 @@ import (
 	"errors"
 )
 
+// Error wraps an inner error, allows adding contextual data to enrich the inner error
+// and provides a friendly message that is safe for external system's consumption.
 type Error struct {
 	inner           error
 	contextual      map[string]any
 	FriendlyMessage string
 }
 
+// Error returns the full error message: the friendly message it contains
+// and it's inner error message.
 func (e *Error) Error() string {
 	if e == nil {
 		return ""
@@ -26,6 +30,7 @@ func (e *Error) Error() string {
 	return msg
 }
 
+// Is asserts if error can be cast to target type.
 func (e *Error) Is(target error) bool {
 	tErr, ok := target.(*Error)
 	if !ok {
@@ -54,6 +59,7 @@ func (e *Error) Is(target error) bool {
 	}
 }
 
+// Unwrap returns the inner error, or nil.
 func (e *Error) Unwrap() error {
 	if e == nil {
 		return nil
@@ -62,6 +68,7 @@ func (e *Error) Unwrap() error {
 	return e.inner
 }
 
+// Wrap wraps an inner error, along with a friendly message.
 func Wrap(inner error, friendlyMessage string, options ...Option) *Error {
 	e := &Error{
 		inner:           inner,
@@ -75,8 +82,10 @@ func Wrap(inner error, friendlyMessage string, options ...Option) *Error {
 	return e
 }
 
+// Option is the optional parameter for Wrap.
 type Option func(e *Error)
 
+// ContextualOption is an Option that adds contextual data to Error using a key/value pair.
 func ContextualOption(key string, value any) Option {
 	return func(e *Error) {
 		e.contextual[key] = value
